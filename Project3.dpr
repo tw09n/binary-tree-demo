b3c2465af1b4cee5e9d2876fb4a31253a3306aa4
tw09n <09n@yahoo.com.tw>
2016-06-30 16:32:56 +0800

    1.Binary Tree

---
+++ Project3.dpr
@@ -0,0 +1,19 @@
+program Project3;
+
+uses
+  madExcept,
+  madLinkDisAsm,
+  madListHardware,
+  madListProcesses,
+  madListModules,
+  Vcl.Forms,
+  Unit3 in 'Unit3.pas' {Form3};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm3, Form3);
+  Application.Run;
+end.
